32d6d9209238083f12e6d4438b327dd10807b023
adithyanr305 <adithyanr346@gmail.com>
2026-07-10 22:44:08 +0530

    Simplify Contains Duplicate using set comparison
    Refactored containsDuplicate method to use set for uniqueness check.

---
+++ Easy/217_Contains_Duplicate.py
@@ -0,0 +1,15 @@
+15 ms:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        return False
+5 ms:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique = set(nums)
+        return len(nums) != len(unique)
+#better solution is to create a set called unique and compare len of unique to len of nums array
